9d19bbd8e31f61727b8744eca57eb445f832bba1
David Suárez Garcia <davidsgarcia1402@gmail.com>
2023-02-15 16:23:56 -0300

    PROJECT TKINTER

--- inventariotkinter.py
+++ inventariotkinter.py
@@ -10,7 +10,6 @@ import os
 import sys
 import babel
 
-#https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
